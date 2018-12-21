cac988118ae8d5f290f10ce85695f4841c611379
DORM-MX <dromo@itexico.net>
2018-12-21 09:15:45 -0600

    Add new view

--- ASPNetCoreReact/ClientApp/src/components/NavMenu.js
+++ ASPNetCoreReact/ClientApp/src/components/NavMenu.js
@@ -32,7 +32,12 @@ export class NavMenu extends Component {
               <NavItem>
                 <Glyphicon glyph='th-list' /> Fetch data
               </NavItem>
-            </LinkContainer>
+                    </LinkContainer>
+                    <LinkContainer to={'/counterdouble'}>
+                        <NavItem>
+                            <Glyphicon glyph='education' /> Counter Double
+              </NavItem>
+                    </LinkContainer>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
